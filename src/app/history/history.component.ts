1658bd674c0171d04f0be64a892071af9ffd9571
Jack Matthews <jackhkmatthews@gmail.com>
2017-10-20 11:16:28 +0100

    Functioning calendar array generator

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -37,6 +37,42 @@ export class HistoryComponent implements OnInit {
     this.route.data.forEach((data: { days: DaysResponse }) => {
       this.days = data.days._embedded.days;
     });
+
+    function generateCalendarArray(DBDays) {
+      let array: any = Array(DBDays.length).fill(null);
+      array = array
+      .map((element, index) => {
+        return new Date(Date.now() - index * (86400000));
+      })
+      .filter(date => {
+        const day = date.toUTCString().split(',')[0];
+        return !(day.includes('Sat') || day.includes('Sun'));
+      })
+      .reduce((sum, date) => {
+        const ISOString = date.toISOString().split('T')[0];
+        if (DBDays[sum.daysIndex].date === ISOString) {
+          sum.calendarArray.push({
+            date,
+            ID: DBDays[sum.daysIndex].ID
+          });
+          sum.daysIndex ++;
+        } else {
+          sum.calendarArray.push({
+            date,
+            ID: null
+          });
+        }
+        return sum;
+      }, {calendarArray: [], daysIndex: 0});
+      return array;
+    }
+
+    console.log(generateCalendarArray(this.days));
+    // console.log(this.days);
+
+
+
+
   }
 
 }
