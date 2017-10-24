b1f4870475ee69661647f8da7eb2d145e197e0ec
Jack Matthews <jackhkmatthews@gmail.com>
2017-10-24 10:26:56 +0100

    small styling improvements and use of angular Date pipe

--- src/app/today/today.component.ts
+++ src/app/today/today.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpParams } from '@angular/common/http';
 
-import { PositionService, SummaryService, StaffMemberService, DayService } from '../api/services';
-
 import { DayItem, DaysResponse } from '../api/models';
 
 @Component({
@@ -12,59 +10,19 @@ import { DayItem, DaysResponse } from '../api/models';
 })
 export class TodayComponent implements OnInit {
   /**
-   * Today's day from backend API.
-   * Null if day doesn't exist yet
+   * Today's Date
    *
-   * @type {number}
+   * @type {Date}
    * @memberof TodayComponent
    */
-  public today: DayItem;
+  public today: Date = new Date();
 
-  constructor(
-    private positionService: PositionService,
-    private summaryService: SummaryService,
-    private staffMemberService: StaffMemberService,
-    private dayService: DayService
-  ) {
-    // positionService.get('be774180-9a2a-11e7-979c-cb40e3c89a8d')
-    // .subscribe(value => console.log(value));
-    // positionService.list(new HttpParams().set('dayID', 'bd76e880-9a2a-11e7-979c-cb40e3c89a8d'))
-    // .subscribe(value => console.log(value));
-    // summaryService.get('be7ee2a2-9a2a-11e7-979c-cb40e3c89a8d')
-    // .subscribe(value => console.log(value));
-    // summaryService.list(new HttpParams().set('dayID', 'bd76e880-9a2a-11e7-979c-cb40e3c89a8d'))
-    // .subscribe(value => console.log(value));
-    // staffMemberService.get('bd53f730-9a2a-11e7-979c-cb40e3c89a8d')
-    // .subscribe(value => console.log(value));
-    // staffMemberService.list(new HttpParams().set('current', 'true'))
-    // .subscribe(value => console.log(value));
-    // dayService.get('bd76e880-9a2a-11e7-979c-cb40e3c89a8d')
-    // .subscribe(value => console.log(value));
-    // dayService.list(new HttpParams().set('date', new Date().toISOString().split('T')[0]))
-    //   .subscribe((value: DaysResponse) => {
-    //     this.today = value._embedded ? value._embedded.days[0] : null;
-    //   });
-    // dayService.list(new HttpParams().set('date', '2017-04-12'))
-    //   .subscribe((value: DaysResponse) => {
-    //     this.today = value._embedded ? value._embedded.days[0] : null;
-    //     if (this.today) {
-    //       this.getStandup(this.today.ID);
-    //     }
-    //   });
+  constructor() {
+
   }
 
   ngOnInit() {
 
-  }
-  /**
-   * Returns standup object
-   *
-   * @readonly
-   * @type {void}
-   * @memberof TodayComponent
-   */
-  public getStandup(dayID: string): void {
-    console.log(dayID);
   }
 
 }
