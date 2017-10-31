b05828031574cbffff2cada02537f73e98672711
Jack Matthews <jackhkmatthews@gmail.com>
2017-10-31 12:46:19 +0000

    onsubmit method for staff update form

--- src/app/staff-member-edit/staff-member-edit.component.ts
+++ src/app/staff-member-edit/staff-member-edit.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { StaffMemberItem } from '../api/models';
+import { StaffMemberService } from '../api/services';
 
 @Component({
   selector: 'app-staff-member-edit',
@@ -17,12 +19,43 @@ export class StaffMemberEditComponent implements OnInit {
    */
   public staffMember: StaffMemberItem;
 
-  constructor(private route: ActivatedRoute) { }
+  /**
+   * FormGroup Object for staff details form
+   * setup in child component
+   *
+   * @type {FormGroup}
+   * @memberOf ProjectCertificatesComponent
+   */
+  public staffMemberDetailsForm: FormGroup;
+
+  /**
+   * Submit form
+   *
+   * @param {positions} positions
+   * @param {summaries} summaries
+   * @memberof HistoryComponent
+   *
+   * @method renderDiagram
+   */
+  public onSubmit() {
+    this.staffMemberService.update(this.staffMember.ID, this.staffMemberDetailsForm.value)
+      .subscribe(value => {
+        this.router.navigateByUrl(`team/${value.ID}`);
+      });
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private staffMemberService: StaffMemberService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.route.data.forEach((data: { staffMember: StaffMemberItem }) => {
       this.staffMember = data.staffMember;
     });
+    this.staffMemberDetailsForm = this.formBuilder.group({});
   }
 
 }
