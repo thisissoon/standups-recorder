120defa6a294d8a2f14f7472b14e70d052812862
Jack Matthews <jackhkmatthews@gmail.com>
2017-11-06 17:22:50 +0000

    created alert service and model

---
+++ src/app/shared/alerts/alert.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { Router, NavigationEnd, NavigationError } from '@angular/router';
+import { Alert } from './alert.model';
+
+/**
+ * Used to add site alerts to be displayed
+ *
+ * @example
+ * ```
+ * let alert: Alert = {
+ *   type: 'success',
+ *   msg: 'Well done, you did a thing!',
+ *   duration: 5000
+ * };
+ * service.add(alert);
+ * ```
+ *
+ * @export
+ * @class AlertService
+ */
+@Injectable()
+export class AlertService {
+  /**
+   * Default animation duration
+   *
+   * @static
+   * @type {number}
+   * @default 500
+   * @memberOf AlertService
+   */
+  public ANIMATION_DURATION = 500;
+  /**
+   * List of alerts to display
+   *
+   * @type {Array<Alert>}
+   * @memberOf AlertService
+   */
+  public alerts: Array<Alert> = [];
+  /**
+   * Add a new alert to list of alerts
+   *
+   * @param {Alert} alert
+   *
+   * @memberOf AlertService
+   */
+  public add(alert: Alert) {
+    console.log(alert);
+    this.alerts.push(alert);
+    console.log(this.alerts);
+    this.setAlertToClose(alert);
+  }
+  /**
+   * Set a timeout to close an alert
+   *
+   * @param {Alert} alert
+   *
+   * @memberOf AlertService
+   */
+  public setAlertToClose(alert: Alert) {
+    const duration = (alert.duration - this.ANIMATION_DURATION),
+      close = () => this.alerts = [];
+    setTimeout(close, duration);
+  }
+
+}
